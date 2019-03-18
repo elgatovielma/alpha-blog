65a4dd0248df55f101896d6d5b48db74118e4739
Alberto <vielmagalberto@gmail.com>
2019-03-18 16:58:04 -0400

    CRUD details

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+    # this will call set_article before these methods are called
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+
     def index
         @articles = Article.all
     end
@@ -19,15 +22,18 @@ class ArticlesController < ApplicationController
     end
 
     def show
-       @article = Article.find(params[:id]) 
+    end
+
+    def destroy
+        @article.destroy
+        flash[:notice] = "Article was successfully deleted"
+        redirect_to articles_path
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice] = "Article was successfully updated"
             redirect_to article_path(@article)
@@ -37,6 +43,11 @@ class ArticlesController < ApplicationController
     end
 
     private
+
+        def set_article
+            @article = Article.find(params[:id])
+        end
+
         def article_params
             params.require(:article).permit(:title, :description)
         end
